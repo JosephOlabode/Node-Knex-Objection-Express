2737f6fb719498697612bd2d8b8013a9dcdc6f48
JosephOlabode <josepholabode17@gmail.com>
2021-07-12 09:27:46 +0000

    connected the knex and objection to the server.js file

--- todo_example/server.js
+++ todo_example/server.js
@@ -3,6 +3,16 @@ const app = express();
 
 const apiRoutes = require("./routes/api");
 
+// implementing the connection with the development db
+const Knex = require('knex');
+const knexFile = require('./knefile.js');
+const knex = Knex(knexFile.development);
+
+
+const {Model} = require('objection');
+
+Model.knex(knex);
+
 app.use("/api", apiRoutes);
 
 
