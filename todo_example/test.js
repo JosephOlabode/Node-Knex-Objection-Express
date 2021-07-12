0c947fcefc6a038861b2373a260350c77cf2a059
JosephOlabode <josepholabode17@gmail.com>
2021-07-12 03:28:32 +0000

    implemented the query builder with objection js

--- todo_example/test.js
+++ todo_example/test.js
@@ -5,15 +5,34 @@ const knexFile = require("./knexfile.js");
 // Make the connection to the database
 const knex = Knex(knexFile.development);
 
-// Run queries
-knex("todos")
-    .where("user_id",  1)
-    .then(rows => {
-        for(row of rows) {
-            console.log(row);
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        throw err;
-    })
+// Import Objection.js Model class
+const {Model} = require("objection");
+
+// Bind all models to the knex instance
+Model.knex(knex);
+
+// Create the User model class
+class User extends Model {
+    static get tableName() {
+        return "users";
+    }
+}
+
+// Run the query in async/await
+const getUsers = async () => {
+    // returns all users
+    const users = await User.query();
+    console.log(users);
+}
+// // Run queries with just pure knex query builder
+// knex("todos")
+//     .where("user_id",  1)
+//     .then(rows => {
+//         for(row of rows) {
+//             console.log(row);
+//         }
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         throw err;
+//     })
