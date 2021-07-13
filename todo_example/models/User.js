2c0717fb114aa5289c20228d555475cc3699dba5
JosephOlabode <josepholabode17@gmail.com>
2021-07-13 00:56:53 +0000

    implemented validation on the User model

--- todo_example/models/User.js
+++ todo_example/models/User.js
@@ -4,6 +4,20 @@ class User extends Model {
     static get tableName() {
         return 'users';
     }
+
+    // jsonSchema used for input validation. This is not the database schema!
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: ['name'],
+
+            properties: {
+                id: {type: 'integer'},
+                name: {type: 'string', minLenth:1, maxLength: 255},
+                age: {type: 'number'} //optional
+            }
+        }
+    }
 }
 
 // Export User Class
