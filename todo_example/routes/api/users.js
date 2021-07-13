07669075640ef6cfbd3ebaf7fae6545203b55d52
JosephOlabode <josepholabode17@gmail.com>
2021-07-13 01:05:29 +0000

    implemented the post route for the users

--- todo_example/routes/api/users.js
+++ todo_example/routes/api/users.js
@@ -4,9 +4,21 @@ const User = require('../../models/User');
 
 router.get('/',  async (req, res) => {
 
-    const users = await User.query().findById(2);
+    const users = await User.query();
     res.json(users);
 });
+
+// create new user using a POST request
+router.post("/", async (req, res) => {
+    try {
+
+        const user = await User.query().insert({name: "Kim", age: 28});
+        res.json(user);
+
+    } catch(err) {
+        res.status(400).json(err.data);
+    }
+})
 
 //Export to api.js
 module.exports = router;
